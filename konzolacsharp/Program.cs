9420db555ceb6ccb192f45667af8c5b1f8f3e7b1
fr4hunnid <filip.ronce@gmail.com>
2023-03-14 16:01:38 +0100

    hgj

--- konzolacsharp/Program.cs
+++ konzolacsharp/Program.cs
@@ -7,6 +7,7 @@ namespace konzolacsharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("AloALO");
         }
     }
 }
